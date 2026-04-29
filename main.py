14eba4a9a302683c1dc85f7defe44b23abfabbe8
Charista <tatacharista89@gmail.com>
2026-04-29 20:13:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # ======================
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/day.csv")
+    df = pd.read_csv("day.csv")
     df["dteday"] = pd.to_datetime(df["dteday"])
 
     # Mapping tahun
